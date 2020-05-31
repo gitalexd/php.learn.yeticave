2a439a31f68f299ce30218a2e4080ed49c49d879
alex <gitalexd>
2020-05-31 19:38:23 +0300

    id add

--- queries.sql
+++ queries.sql
@@ -1,25 +1,25 @@
 
 
 INSERT INTO `category` (`name`, `code`) VALUES
-('Ботинки', 'boots'),
-('Доски и лыжи', 'boards'),
-('Инструменты', 'tools'),
-('Крепления', 'attachment'),
-('Одежда', 'clothing'),
-('Разное', 'other')
+(1, 'Ботинки', 'boots'),
+(2, 'Доски и лыжи', 'boards'),
+(3, 'Инструменты', 'tools'),
+(4, 'Крепления', 'attachment'),
+(5, 'Одежда', 'clothing'),
+(6, 'Разное', 'other')
 
 INSERT INTO `lot` (`created_at`, `name`, `description`, `img_url`, `price`, `expire_data`, `rate_step`) VALUES
-('2020-05-21', '2014 Rossignol District Snowboard', 'Лучший сноуборд в районе', 'img/lot-1.jpg', 10999, '2020-05-22', 10000),
-('2020-05-21', 'DC Ply Mens 2016/2017 Snowboard', 'Лучший сноуборд в городе', 'img/lot-2.jpg', 159999, '2020-05-22', 10000),
-('2020-05-21', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Лучший сноуборд в городе', 'img/lot-3.jpg', 8000, '2020-05-22', 10000),
-('2020-05-21', 'Ботинки для сноуборда DC Mutiny Charocal', 'Супер ботинки', 'img/lot-4.jpg', 10099, '2020-05-22', 11000 ),
-('2020-05-21', 'Куртка для сноуборда DC Mutiny Charocal', 'Супер куртка', 'img/lot-5.jpg', 7500, '2020-05-23', 8000 ),
-('2020-05-21', 'Маска Oakley Canopy', 'Супер маска', 'img/lot-6.jpg', 5400, '2020-05-24', 6000 )
+(1, '2020-05-21', '2014 Rossignol District Snowboard', 'Лучший сноуборд в районе', 'img/lot-1.jpg', 10999, '2020-05-22', 10000),
+(2, '2020-05-21', 'DC Ply Mens 2016/2017 Snowboard', 'Лучший сноуборд в городе', 'img/lot-2.jpg', 159999, '2020-05-22', 10000),
+(3, '2020-05-21', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Лучший сноуборд в городе', 'img/lot-3.jpg', 8000, '2020-05-22', 10000),
+(4, '2020-05-21', 'Ботинки для сноуборда DC Mutiny Charocal', 'Супер ботинки', 'img/lot-4.jpg', 10099, '2020-05-22', 11000 ),
+(5, '2020-05-21', 'Куртка для сноуборда DC Mutiny Charocal', 'Супер куртка', 'img/lot-5.jpg', 7500, '2020-05-23', 8000 ),
+(6, '2020-05-21', 'Маска Oakley Canopy', 'Супер маска', 'img/lot-6.jpg', 5400, '2020-05-24', 6000 )
 
 INSERT INTO `rate` (`placing_at`, `proposition_sum`) VALUES
-('2020-05-22', 12000),
-('2020-05-22', 13000)
+(1, '2020-05-22', 12000),
+(2, '2020-05-22', 13000)
 
 INSERT INTO `user` (`date_of_registration`, `email`, `name`, `password`, `contacts`) VALUES
-('2020-01-01', '123', 'Missi', 'missi1', '89789789786'),
-('2020-01-01', 'missi@mail.ru', 'Missi', 'missi1', '89789789786')
+(1, '2020-01-01', '123', 'Missi', 'missi1', '89789789786'),
+(2, '2020-01-01', 'missi@mail.ru', 'Missi', 'missi1', '89789789786')
--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `category` (
+    `id` int NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20),
   `code` VARCHAR(20),
-  PRIMARY KEY (`code`)
+  PRIMARY KEY (`id`)
 )
 
 CREATE TABLE `lot` (
+    `id` int NOT NULL AUTO_INCREMENT,
   `created_at` date,
   `name` VARCHAR (50),
   `description` VARCHAR (50),
@@ -12,21 +14,23 @@ CREATE TABLE `lot` (
   `price` int,
   `expire_data` date,
   `rate_step` int,
-  PRIMARY KEY (`name`)
+  PRIMARY KEY (`id`)
 )
 
 CREATE TABLE `rate` (
+`id` int NOT NULL AUTO_INCREMENT,
 `placing_at` VARCHAR(30),
 `proposition_sum` int,
-  PRIMARY KEY (`proposition_sum`)
+  PRIMARY KEY (`id`)
 )
 
 CREATE TABLE `user` (
+`id` int NOT NULL AUTO_INCREMENT,
 `date_of_registration` date,
 `email` VARCHAR(50),
 `name` VARCHAR(50),
 `password` VARCHAR(50),
 `contacts` VARCHAR(50),
-  PRIMARY KEY (`email`)
+  PRIMARY KEY (`id`)
 )
 
